a805e1796e101a0c866d6d11ecdf3c64e1f0c85f
mabgcm <bugucam@gmail.com>
2022-10-30 14:35:14 +0700

    another attampt

--- yba/settings.py
+++ yba/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-LOGIN_REDIRECT_URL = "/"
+# LOGIN_REDIRECT_URL = "/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
