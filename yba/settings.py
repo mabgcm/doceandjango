702d1e9e2afa1db6bb862ea205b46aad94e4c82b
mabgcm <bugucam@gmail.com>
2022-10-30 13:04:23 +0700

    second commit

--- yba/settings.py
+++ yba/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '+$)aap86$f$-k&splcl8my)@um%j_b4p3c@lmbpyvztlf0#7mj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
